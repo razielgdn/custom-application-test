61e1ad28cd726ea62245a7c4f365c732336734a9
Octavio Guendulain <octavioraziel.gdn@gmail.com>
2024-04-11 00:50:15 -0600

    udate files to compile a recipe

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+    while (1){
+        printf ("Hello, this is a test application\n");
+        printf ("Hello, this line was added to test the online repo\n");
+        sleep(1);
+    }
+}
